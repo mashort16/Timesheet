49ba8301fdd3ae5d80241c4d409ac301b8ee01dc
NIC NOVELLI <NJ.NOVELLI@GMAIL.OM>
2016-11-09 11:17:52 -0500

    Fixed login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
+  devise_scope :user do
+  	root :to => 'devise/sessions#new'
+  end
+
   get 'users/index'
   get 'users/create'
   get 'users/update'
